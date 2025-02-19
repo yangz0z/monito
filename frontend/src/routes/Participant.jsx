32db3636866601fcf5e55b0aab7542da1d550266
moodiseo <moodiseo@gmail.com>
2025-02-19 17:45:58 +0900

    Participant.jsx update

--- frontend/src/routes/Participant.jsx
+++ frontend/src/routes/Participant.jsx
@@ -7,17 +7,26 @@ export default function Participant() {
   const [participants, setParticipants] = useState([]);
   const [name, setName] = useState("");
   const [contact, setContact] = useState("");
+  const [errors, setErrors] = useState({ name: false, contact: false });
+
+  const handleNext = () => {
+    let newErrors = { eventName: false, budget: false };
+
+    if (!name.trim()) newErrors.name = true;
+    if (!contact.trim()) newErrors.contact = true;
+
+    setErrors(newErrors);
+  };
 
   // 참가자 추가 (submit 이벤트 핸들러)
   const handleSubmit = (e) => {
-    e.preventDefault(); // 폼 제출 시 새로고침 방지
+    e.preventDefault();
     if (name && contact) {
       setParticipants([...participants, { name, contact }]);
       setName("");
       setContact("");
     }
   };
-
   // 참가자 삭제
   const handleRemoveParticipant = (index) => {
     setParticipants(participants.filter((_, i) => i !== index));
@@ -42,10 +51,15 @@ export default function Participant() {
           <input
             type="text"
             placeholder="이름"
-            className="border shadow rounded-md pl-2 px-6 py-1.5 text-gray-700 w-80"
+            className={`border shadow rounded-md pl-2 px-6 py-1.5 text-gray-700 w-80 ${
+              errors.name ? "border-red-500" : ""
+            }`}
             value={name}
             onChange={(e) => setName(e.target.value)}
           />
+          {errors.name && (
+            <p className="text-red-500 text-xs mt-1">필수 입력 항목입니다.</p>
+          )}
         </div>
 
         {/* 이메일 또는 휴대폰 번호 입력 */}
@@ -53,15 +67,19 @@ export default function Participant() {
           <input
             type="text"
             placeholder="이메일 또는 휴대폰 번호"
-            className="border shadow rounded-md pl-2 px-6 py-1.5 text-gray-700 w-80"
+            className={`border shadow rounded-md pl-2 px-6 py-1.5 text-gray-700 w-80 ${errors.contact ? "border-red-500" : ""}`}
             value={contact}
             onChange={(e) => setContact(e.target.value)}
           />
+
           {/* 제출 버튼을 Form 내부의 submit 버튼으로 변경 */}
           <button type="submit" className="ml-2">
             <FaPlusCircle className="text-3xl text-[#FF8F00]" />
           </button>
         </div>
+        {errors.contact && (
+          <p className="text-red-500 text-xs mt-1">필수 입력 항목입니다.</p>
+        )}
       </form>
 
       {/* 참가자 목록 */}
@@ -83,7 +101,7 @@ export default function Participant() {
 
       {/* 다음 버튼 */}
       <button
-        onClick={handleSubmit}
+        onClick={handleNext}
         className="mt-9 px-8 py-3 text-white bg-[#D32F2F] shadow-xl rounded-full hover:bg-red-700 font-semibold"
       >
         다음
