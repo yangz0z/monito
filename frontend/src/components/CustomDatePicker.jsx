4fb4bb3993a6086a3aeb48e5b231704e278f24e1
moodiseo <moodiseo@gmail.com>
2025-03-19 17:30:02 +0900

    CustomDatePicker Update.

--- frontend/src/components/CustomDatePicker.jsx
+++ frontend/src/components/CustomDatePicker.jsx
@@ -9,25 +9,29 @@ export default function CustomDatePicker({ selectedDate, setSelectedDate }) {
   return (
     <div className="mt-2 flex items-center justify-between w-64 relative overflow-visible">
       <span className="text-sm font-semibold">{t("eventDate")}</span>
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-        dateFormat="yyyy-MM-dd"
-        className="border shadow rounded-md w-24 py-1.5 text-gray-700 text-center text-sm font-semibold"
-        withPortal
-        popperProps={{
-          modifiers: [
-            {
-              name: "preventOverflow",
-              options: { boundary: "viewport" },
-            },
-            {
-              name: "flip",
-              options: { fallbackPlacements: ["top", "bottom"] },
-            },
-          ],
-        }}
-      />
+      <div className="absolute right-0">
+        {" "}
+        {/* 추가된 div */}
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="yyyy-MM-dd"
+          className="border shadow rounded-md w-24 py-1.5 text-gray-700 text-center text-sm font-semibold"
+          popperProps={{
+            modifiers: [
+              {
+                name: "preventOverflow",
+                options: { boundary: "viewport" },
+              },
+              {
+                name: "flip",
+                options: { fallbackPlacements: ["top", "bottom"] },
+              },
+            ],
+          }}
+          popperPlacement="bottom-end" // <--- 오른쪽 정렬 강제
+        />
+      </div>
     </div>
   );
 }
